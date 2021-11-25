50ec0425392e98df4e1776e05c728222224bd685
AppSpector CI <heroes@techery.io>
2021-11-26 01:02:45 +0200

    Direct url

--- Package.swift
+++ Package.swift
@@ -20,17 +20,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppSpectorSDK",
-            path: "AppSpectorSDK/AppSpectorSDK.xcframework.zip",
+            url: "https://github.com/techery/ios-sdk-spm/raw/1.4.5/AppSpectorSDK/AppSpectorSDK.xcframework.zip",
             checksum: "19eeb4c08654599ef9a9c14340d8c9caf3c2fcc25eeb4f25e17152897b030e4c"
         ),
         .binaryTarget(
             name: "AppSpectorSDKE2E",
-            path: "AppSpectorSDKE2E/AppSpectorSDKE2E.xcframework.zip",
+            url: "https://github.com/techery/ios-sdk-spm/raw/1.4.5/AppSpectorSDK/AppSpectorSDKE2E.xcframework.zip",
             checksum: "78064690dcd0a7127ac8e013ec29ca0b4ec61620b31921d181b505059c3520b8"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
-            path: "AppSpectorTVSDK/AppSpectorTVSDK.xcframework.zip",
+            url: "https://github.com/techery/ios-sdk-spm/raw/1.4.5/AppSpectorSDK/AppSpectorTVSDK.xcframework.zip",
             checksum: "6a0f5e42ef6c938f3160bf347e182c8afb63a6031463aa3ef1a711220e5bd0da"
         )
     ]
